7284247986103f5817eecd247f984cfd09bba142
giselymarques <100145084+giselymarques@users.noreply.github.com>
2022-02-21 12:29:30 -0300

    Add files via upload

---
+++ Código-Gisely.js
@@ -0,0 +1,19 @@
+
+    
+    <!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+  var array= [1,2,3,4,5,6,7,8,9,10]
+contador=0
+for ( var num of array) {
+    
+    console.log('5x' +'='+num*5)
+        contador++
+
+}
+   
+  </head>
+  <body>
+    Aqui vai o código HTML que fará seu site aparecer.
+  </body>
+</html>
